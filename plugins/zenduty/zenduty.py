580a7b7bf58b4d3ad5246978156f8bbb266a4d11
gulshantelkar <99161604+gulshantelkar@users.noreply.github.com>
2024-09-18 16:09:42 +0530

    Update zenduty.py

--- plugins/zenduty/zenduty.py
+++ plugins/zenduty/zenduty.py
@@ -10,10 +10,10 @@ except ImportError:
     from alerta.app import app  # alerta < 5.0
 
 LOG = logging.getLogger("alerta.plugins.zenduty")
-DASHBOARD_URL = os.environ.get(
-    'DASHBOARD_URL') or app.config['DASHBOARD_URL']
+DASHBOARD_ID= os.environ.get(
+    'DASHBOARD_ID') or app.config['DASHBOARD_ID']
 
-ZENDUTY_EVENTS_URL = "https://d344-103-174-71-214.ngrok-free.app/api/appsync/webhook/DASHBOARD_URL/"
+ZENDUTY_EVENTS_URL = "https://d344-103-174-71-214.ngrok-free.app/api/appsync/webhook/DASHBOARD_ID/"
 
 
 class TriggerEvent(PluginBase):
